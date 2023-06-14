fac8f97c5142c7a10ad99d5a94b5b3ecf8b57f09
GuidoNicolini <guidonicolini123@hotmail.com>
2023-06-13 23:54:53 -0300

    fix services

---
+++ personalresumefront/src/app/educacion/interfaces/educacion.ts
@@ -0,0 +1,9 @@
+export interface Education {
+    id:            string;
+    degree:        string;
+    description:   string;
+    establishment: string;
+    logo:          string;
+    isDelete:      boolean;
+    profile:       string;
+}
